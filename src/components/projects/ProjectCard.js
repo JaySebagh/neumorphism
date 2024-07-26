75d31f63f510d58c0e9244df13641838a2c0531b
dev <jaysebagh@gmail.com>
2024-07-26 14:26:52 -0700

    projects tab functionality and majority of styling complete

---
+++ src/components/projects/ProjectCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../../assets/styles/projectcard.css'
+
+const ProjectCard = ({ data } ) => {
+    return(
+        <div class="card">
+            <img className="url" src={data.url} alt="project preview"/>
+            <div className="title-container">
+                <p className="title">{data.title}</p>
+                <a className="repo-button" href={`//${data.github}`} target="_blank" rel="noreferrer">
+                    REPO
+                </a>
+                <a className="demo-button" href={`//${data.demo}`} target="_blank" rel="noreferrer">
+                    DEMO
+                </a>
+            </div>
+            <p className="bio">{data.bio}</p>
+            <p className="stack">{data.stack}</p>
+        </div>
+    )
+};
+
+export default ProjectCard;
--- src/components/tabs/Projects.js
+++ src/components/tabs/Projects.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import '../../assets/styles/projects.css'
+import projectsDATA from '../../assets/data/projectsData.json';
+import ProjectCard from '../projects/ProjectCard';
+
 
 const Projects = () => {
+    const [search, setSearch] = useState("");
+
     return(
-        <div>
-            Projects
+        <div className="projects-tab-container">
+            <input
+                type="text"
+                autocomplete="off"
+                className="searchbar"
+                placeholder="Search Project Title"
+                value={ search }
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <div className="projects-container">
+                <div>
+                    {projectsDATA.filter((item) =>
+                        item.title.toLowerCase().includes(search.toLowerCase())
+                    ).map((data, index) => (
+                        <ProjectCard key={index} data={data}/>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 };
